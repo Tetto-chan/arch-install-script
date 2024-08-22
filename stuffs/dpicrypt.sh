7bdb1f222ec9b45dbc49fa9b568431adbaa0ff4c
Tetto-chan <2000nazarbro@gmail.com>
2024-08-22 13:59:21 +0700

    update past contribute

---
+++ stuffs/dpicrypt.sh
@@ -0,0 +1,9 @@
+cd /opt
+git clone https://github.com/bol-va n/zapret.git
+cd zapret
+./install_bin.sh
+./install_prereq.sh
+./blockcheck.sh
+./install_easy.sh
+echo "hiding traffic was successful"
+
